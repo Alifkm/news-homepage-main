adca69dac62ca2bb0fd9931945dbd104b6065c8c
Alifkm <alifkahfi@gmail.com>
2022-11-23 09:15:31 +0700

    fix: navbar button now can work on mobile layout

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
-
-  if($( window ).width() >= 992) {
-    $('.nav__menu').removeClass('active');
-    $('.nav__bar-wrapper').removeClass('active');
-  }else {
-    $('#bar').click(function() {
-      $('.nav__menu').toggleClass('active');
-      $('.nav__bar-wrapper').toggleClass('active');
-    })
-  }
-
+  $('#bar').click(function() {
+    $('.nav__menu').toggleClass('active');
+    $('.nav__bar-wrapper').toggleClass('active');
+  })
 });
